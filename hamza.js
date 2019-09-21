9ba7bb8237045c251d8eed66432f6f4c8b471f1f
Hamza <you@example.com>
2019-09-21 14:42:48 +0200

    deleting merge

--- hamza.js
+++ hamza.js
@@ -11,10 +11,6 @@ function line(ten){
     console.log(10);
 }
 
-function merge(request){
-    console.log(request);
-}
-
 // reader.question("What is your name? ", (name) => {
 //   console.log(`Hello ${name}!`);
 //
